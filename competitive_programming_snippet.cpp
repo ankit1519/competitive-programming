62be5c8e9411ca422114dbb2bee42a7b37be5836
Ankit Raj <raazankit1502@gmail.com>
2023-12-10 04:36:27 +0530

    Create competitive_programming_snippet.cpp

---
+++ competitive_programming_snippet.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define vii vector<int,int>
+#define int long long int
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+
+using namespace std;
+
+
+int32_t main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+	return 0;
+}
